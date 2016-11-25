3f2a66076a71eb53a372b5e2af9165559fea3693
Maximilian Bügler <max@maxbuegler.eu>
2016-11-25 16:46:41 +0100

    Restructuring

--- src/pedometer.js
+++ src/pedometer.js
@@ -1,27 +1,22 @@
-var synaptic = require('synaptic');
+/*===========================================================================*\
+ * Pedometer algorithm for node.js
+ *
+ * Uses autocorrelation and neural networks for robust step detection.
+ *
+ * (c) 2016 Maximilian Bügler
+ *
+ *===========================================================================*/
+
+
 var parse = require('csv-parse/lib/sync');
 var fs = require('fs');
-
-var neuron = synaptic.Neuron,
-    layer = synaptic.Layer,
-    network = synaptic.Network,
-    trainer = synaptic.Trainer,
-    architect = synaptic.Architect;
-
-
+var synaptic = require('synaptic');
 var neuralnetwork=network.fromJSON(JSON.parse(fs.readFileSync('./data/neuralnetwork.json','utf8')));
 
-var stepdata=fs.readFileSync('./data/steps.csv','utf8');
-stepdata=parse(stepdata, {trim: true, auto_parse: true,relax_column_count:true });
-
-var inputsize=20;
-var i=100;
-    var inputdata=[];
-    for (var j=0;j<inputsize;j++){
-        var scaledPosition=j*stepdata[i].length/inputsize;
-        //Linear interpolation
-        inputdata[j]=stepdata[i][Math.floor(scaledPosition)]*(1-(scaledPosition%1))+stepdata[i][Math.ceil(scaledPosition)]*(scaledPosition%1);
+module.exports = {
+    pedometer: function pedometer(acceleration, orientation, samplingrate){
+        
+        
     }
-console.log(inputdata);
-console.log(neuralnetwork.activate(inputdata));
+};
 
---
+++ src/testneuralnetwork.js
@@ -0,0 +1,25 @@
+var parse = require('csv-parse/lib/sync');
+var fs = require('fs');
+
+var neuron = synaptic.Neuron,
+    layer = synaptic.Layer,
+    network = synaptic.Network,
+    trainer = synaptic.Trainer,
+    architect = synaptic.Architect;
+
+
+var neuralnetwork=network.fromJSON(JSON.parse(fs.readFileSync('./data/neuralnetwork.json','utf8')));
+
+var stepdata=fs.readFileSync('./data/steps.csv','utf8');
+stepdata=parse(stepdata, {trim: true, auto_parse: true,relax_column_count:true });
+
+var inputsize=20;
+var i=100;
+    var inputdata=[];
+    for (var j=0;j<inputsize;j++){
+        var scaledPosition=j*stepdata[i].length/inputsize;
+        //Linear interpolation
+        inputdata[j]=stepdata[i][Math.floor(scaledPosition)]*(1-(scaledPosition%1))+stepdata[i][Math.ceil(scaledPosition)]*(scaledPosition%1);
+    }
+console.log(inputdata);
+console.log(neuralnetwork.activate(inputdata));
